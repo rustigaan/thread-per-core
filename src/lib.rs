5128423d692a1b9714a9ab74d8b7cfa21384f975
Jeroen van Maanen <jeroen@leialearns.org>
2025-05-05 21:36:36 +0200

    Added spawning of threads and pinning them with `core_affinity`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-use anyhow::Result;
+use std::thread;
+use anyhow::{anyhow, Result};
 use clap::Parser;
+use core_affinity::{get_core_ids,set_for_current};
 use log::debug;
 
 #[derive(Parser,Debug)]
@@ -13,6 +15,25 @@ struct Arguments {
 pub fn thread_per_core() -> Result<()> {
     let args = Arguments::parse();
     debug!("args: {args:?}");
+
+    let core_ids = get_core_ids().ok_or(anyhow!("No cores found"))?;
+    debug!("core ids: {core_ids:?}");
+
+    let handles = core_ids.into_iter().map(|id| {
+        thread::spawn(move || {
+            // Pin this thread to a single CPU core.
+            let pinned = set_for_current(id);
+            if pinned {
+                debug!("Thread for core {id:?}");
+            } else {
+                debug!("Failed to pin thread to core {id:?}");
+            }
+        })
+    }).collect::<Vec<_>>();
+
+    for handle in handles.into_iter() {
+        handle.join().unwrap();
+    }
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use log::info;
+use log::{error, info};
 use std::error::Error;
 use thread_per_core::thread_per_core;
 
@@ -7,7 +7,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
     info!("Thread per core starting");
 
-    thread_per_core()?;
+    if let Err(error) = thread_per_core() {
+        error!("Error: {error:?}");
+    }
 
     Ok(())
 }
